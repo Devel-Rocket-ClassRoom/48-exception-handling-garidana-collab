c6691d252f84c40e31090728b6c6d894f6c0f816
garidana-collab <garidana@gmail.com>
2026-03-14 11:39:05 +0900

    48
    Game Item excepion in progress

---
+++ GameItemException/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Inventory
+{
+    int MaxCapacity;
+    public Inventory(int cap)
+    {
+        MaxCapacity = cap;
+    }
+    List<string> items;
+
+    public void AddIItem(string itemName)
+    {
+        items.Add(itemName);
+    }
+
+    public void RemoveItem(string itemName)
+    {
+        
+    }
+    public void ShowItem()
+    {
+
+    }
+}
